76d303ad4c7a5ebbe99914fee0aa10f9a1bb87bc
sora <kacky1210@gmail.com>
2021-10-06 22:19:17 +0900

    Add: create a project

---
+++ app/src/main/java/com/websarva/wings/android/flat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.flat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
