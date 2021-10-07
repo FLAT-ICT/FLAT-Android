1bab4f77813d9870fe6aacdcb0b36d207bd26c2f
sora <kacky1210@gmail.com>
2021-10-07 15:10:10 +0900

    Add: FABによるID入力画面への遷移を実装

--- app/src/main/java/com/websarva/wings/android/flat/view/FriendListFragment.kt
+++ app/src/main/java/com/websarva/wings/android/flat/view/FriendListFragment.kt
@@ -6,15 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.websarva.wings.android.flat.R
+import com.websarva.wings.android.flat.databinding.FragmentFriendListBinding
 
 class FriendListFragment : Fragment() {
+
+    private var _binding: FragmentFriendListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend_list, container, false)
+        _binding = FragmentFriendListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabAddFriend.setOnClickListener{
+            parentFragmentManager.beginTransaction().addToBackStack(null).replace(R.id.app_container, AddFriendFragment()).commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
