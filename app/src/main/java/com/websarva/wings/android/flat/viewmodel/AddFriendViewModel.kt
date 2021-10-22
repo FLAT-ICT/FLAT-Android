8f12be0ad148959cda14b0e1a2f54ca5a99d9f98
sora <kacky1210@gmail.com>
2021-10-22 13:01:18 +0900

    Add: 名前検索の通信を行う関数の呼び出しを行う関数を作った

--- app/src/main/java/com/websarva/wings/android/flat/viewmodel/AddFriendViewModel.kt
+++ app/src/main/java/com/websarva/wings/android/flat/viewmodel/AddFriendViewModel.kt
@@ -3,6 +3,7 @@ package com.websarva.wings.android.flat.viewmodel
 import android.util.Log
 import androidx.lifecycle.*
 import com.websarva.wings.android.flat.api.PostData.PostFriends
+import com.websarva.wings.android.flat.api.ResponseData.ResponseSearchUsers
 import com.websarva.wings.android.flat.api.ResponseData.ResponseCheckFriend
 import com.websarva.wings.android.flat.repository.ApiRepository
 import kotlinx.coroutines.Dispatchers
@@ -11,6 +12,12 @@ import java.lang.Exception
 
 class AddFriendViewModel: ViewModel() {
     private val repository = ApiRepository.instance
+
+    //TODO::後で下のmyIDを削除して命名を変更する
+    private val id: Int = 1
+
+    private val _users = MutableLiveData<ResponseSearchUsers>()
+    val users: LiveData<ResponseSearchUsers> get() = _users
 
     //TODO::repositoryでroomか何かと繋いで自分のIDを取ってくるようにする？
     private val _myId = MutableLiveData("000001")
@@ -27,6 +34,25 @@ class AddFriendViewModel: ViewModel() {
 
     private val _errorMessage = MutableLiveData<String>()
     val errorMessage: LiveData<String> get() = _errorMessage
+
+
+    fun getSearchUsers(targetName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = repository.searchUsers(id, targetName)
+                _getCode.postValue(response.code())
+                if (response.isSuccessful) {
+                    Log.d("getSuccess", "${response}\n${response.body()}")
+                    _users.postValue(response.body())
+                } else {
+                    Log.d("getFailure", "$response")
+                }
+            } catch (e: Exception) {
+                _errorMessage.postValue(e.message)
+                e.printStackTrace()
+            }
+        }
+    }
 
     fun getCheckFriend(targetId: String) {
         viewModelScope.launch(Dispatchers.IO) {
