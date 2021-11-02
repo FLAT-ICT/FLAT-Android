654867d1a7ec3e8c716c092f5978ef950369bde8
sora <kacky1210@gmail.com>
2021-11-02 12:39:10 +0900

    Add: 初回起動画面を追加

---
+++ app/src/main/java/com/websarva/wings/android/flat/view/StartupFragment.kt
@@ -0,0 +1,26 @@
+package com.websarva.wings.android.flat.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.websarva.wings.android.flat.R
+
+class StartupFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_startup, container, false)
+    }
+
+}
