1198d7b64bcf57aab470aee146ef0a27cefeb7ae
sora <kacky1210@gmail.com>
2021-11-04 23:25:29 +0900

    Add: Serviceクラスを追加し、試しにライブラリのサンプルコードのうち必要そうな部分を記入

---
+++ app/src/main/java/com/websarva/wings/android/flat/BeaconService.kt
@@ -0,0 +1,103 @@
+package com.websarva.wings.android.flat
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.Observer
+import org.altbeacon.beacon.*
+import org.altbeacon.bluetooth.BluetoothMedic
+
+class BeaconService : Service() {
+    lateinit var region: Region
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val beaconManager = BeaconManager.getInstanceForApplication(this)
+
+        beaconManager.beaconParsers.clear()
+        beaconManager.beaconParsers.add(
+            BeaconParser().
+            setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
+
+        BeaconManager.setDebug(true)
+
+        BluetoothMedic.getInstance().enablePowerCycleOnFailures(this)
+        BluetoothMedic.getInstance().enablePeriodicTests(this, BluetoothMedic.SCAN_TEST + BluetoothMedic.TRANSMIT_TEST)
+
+        setupForegroundService()
+        beaconManager.setEnableScheduledScanJobs(false)
+        beaconManager.backgroundBetweenScanPeriod = 0
+        beaconManager.backgroundScanPeriod = 1100
+
+        region = Region("radius-uuid", null, null, null)
+        beaconManager.startMonitoring(region)
+        beaconManager.startRangingBeacons(region)
+
+        val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(region)
+
+        regionViewModel.regionState.observeForever( centralMonitoringObserver)
+
+        regionViewModel.rangedBeacons.observeForever( centralRangingObserver)
+    }
+
+    private fun setupForegroundService() {
+        val builder = Notification.Builder(this, "BeaconReferenceApp")
+        builder.setSmallIcon(R.drawable.ic_launcher_background)
+        builder.setContentTitle("Scanning for Beacons")
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE
+        )
+        builder.setContentIntent(pendingIntent)
+        val channel =  NotificationChannel("beacon-ref-notification-id",
+            "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT)
+        channel.description = "My Notification Channel Description"
+        val notificationManager =  getSystemService(
+            Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+        builder.setChannelId(channel.id)
+        BeaconManager.getInstanceForApplication(this).enableForegroundServiceScanning(builder.build(), 456)
+    }
+
+    private val centralMonitoringObserver = Observer<Int> { state ->
+        if (state == MonitorNotifier.OUTSIDE) {
+            Log.d("centralMonitoringObserver", "outside beacon region: "+region)
+        }
+        else {
+            Log.d("centralMonitoringObserver", "inside beacon region: "+region)
+            sendNotification()
+        }
+    }
+
+    private val centralRangingObserver = Observer<Collection<Beacon>> { beacons ->
+        Log.d("RangingObserver", "Ranged: ${beacons.count()} beacons")
+        for (beacon: Beacon in beacons) {
+            Log.d("RangingObserverBeacons", "$beacon about ${beacon.distance} meters away")
+        }
+    }
+
+    private fun sendNotification() {
+        val builder = NotificationCompat.Builder(this, "beacon-ref-notification-id")
+            .setContentTitle("Beacon Reference Application")
+            .setContentText("A beacon is nearby.")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+        val stackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntent(Intent(this, MainActivity::class.java))
+        val resultPendingIntent = stackBuilder.getPendingIntent(
+            0,
+            PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE
+        )
+        builder.setContentIntent(resultPendingIntent)
+        val notificationManager =
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, builder.build())
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+}
