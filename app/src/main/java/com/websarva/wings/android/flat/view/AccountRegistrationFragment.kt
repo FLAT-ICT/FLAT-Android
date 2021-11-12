88d2a2e193ae260737df3b7864f21994978ec875
sora <kacky1210@gmail.com>
2021-11-13 00:43:43 +0900

    Add: ユーザー登録用のフラグメントを追加

---
+++ app/src/main/java/com/websarva/wings/android/flat/view/AccountRegistrationFragment.kt
@@ -0,0 +1,31 @@
+package com.websarva.wings.android.flat.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.websarva.wings.android.flat.R
+import com.websarva.wings.android.flat.databinding.FragmentAccountRegistrationBinding
+import com.websarva.wings.android.flat.viewmodel.AccountRegistrationViewModel
+
+class AccountRegistrationFragment : Fragment() {
+    private val viewModel: AccountRegistrationViewModel by viewModels()
+    private var _bindnig: FragmentAccountRegistrationBinding? = null
+    private val binding get() = _bindnig!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _bindnig = FragmentAccountRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/websarva/wings/android/flat/viewmodel/AccountRegistrationViewModel.kt
@@ -0,0 +1,7 @@
+package com.websarva.wings.android.flat.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class AccountRegistrationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
