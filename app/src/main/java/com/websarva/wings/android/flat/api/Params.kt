9c4079f348d9d0a8b4d9298f42b8981c03e17ffa
sora <kacky1210@gmail.com>
2021-11-15 17:23:48 +0900

    Update: BaseURLをエミュレーターを起動しているPCのローカルホストに変更

--- app/src/main/java/com/websarva/wings/android/flat/api/Params.kt
+++ app/src/main/java/com/websarva/wings/android/flat/api/Params.kt
@@ -2,7 +2,8 @@ package com.websarva.wings.android.flat.api
 
 class Params {
     companion object {
-        const val BASE_URL = "http://34.68.157.198:8080"
+//        const val BASE_URL = "http://34.68.157.198:8080"
 //        const val BASE_URL = testPath
+        const val BASE_URL = "http://10.0.2.2:3000"
     }
 }
