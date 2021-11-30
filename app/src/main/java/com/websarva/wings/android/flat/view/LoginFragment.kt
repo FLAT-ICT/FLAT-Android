a1bbdc36e3108768ebd795915d2166f4aff389f7
sora <kacky1210@gmail.com>
2021-11-30 23:12:38 +0900

    Add: 通信が絡まない処理を一通り

--- app/src/main/java/com/websarva/wings/android/flat/view/LoginFragment.kt
+++ app/src/main/java/com/websarva/wings/android/flat/view/LoginFragment.kt
@@ -1,11 +1,14 @@
 package com.websarva.wings.android.flat.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.websarva.wings.android.flat.R
 import com.websarva.wings.android.flat.databinding.FragmentLoginBinding
 import com.websarva.wings.android.flat.viewmodel.LoginViewModel
 
@@ -13,6 +16,7 @@ class LoginFragment : Fragment() {
     private val viewModel: LoginViewModel by viewModels()
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var input: LoginViewModel.LoginInput
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,4 +26,60 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btLogin.setOnSafeClickListener {
+                input = LoginViewModel.LoginInput(
+                    name = etInputNickname.text.toString(),
+                    password = etInputPassword.text.toString(),
+                    isNameOk = false,
+                    isPasswordOk = false
+                )
+                viewModel.checkAndTrim(input)
+            }
+        }
+
+        viewModel.checkResult.observe(viewLifecycleOwner, {
+            input = it
+            if (it.isNameOk) {
+                binding.tilInputNickname.apply {
+                    isErrorEnabled = false
+                }
+            }
+            else if (!it.isNameOk) {
+                binding.tilInputNickname.apply {
+                    error = getString(R.string.input_nickname_error)
+                    isErrorEnabled = true
+                }
+            }
+            if (it.isPasswordOk) {
+                binding.tilInputPassword.apply {
+                    isErrorEnabled = false
+                }
+            }
+            else if (!it.isPasswordOk) {
+                binding.tilInputPassword.apply {
+                    error = getString(R.string.input_password_length_error)
+                    isErrorEnabled = true
+                }
+            }
+            if (it.isNameOk && it.isPasswordOk) {
+                //TODO: is_loggedinのPOST
+                Log.d("login", "loginOk")
+            }
+        })
+
+        //TODO: is_loggedinの結果を監視し、ダイアログ表示orログイン
+
+        //TODO: loginの結果を監視し、エラー表示orルームに格納
+
+        //TODO: ルームに格納できたのを確認し、画面遷移
+
+        binding.tvToAccountRegistration.setOnSafeClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToAccountRegistrationFragment()
+            view.findNavController().navigate(action)
+        }
+    }
 }
--- app/src/main/java/com/websarva/wings/android/flat/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/websarva/wings/android/flat/viewmodel/LoginViewModel.kt
@@ -1,7 +1,44 @@
 package com.websarva.wings.android.flat.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.hadilq.liveevent.LiveEvent
+import com.websarva.wings.android.flat.FLATApplication
+import com.websarva.wings.android.flat.repository.ApiRepository
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val apiRepository = ApiRepository.instance
+    private val roomRepository = FLATApplication.userRoomRepository
+
+    data class LoginInput(
+        var name: String,
+        val password: String,
+        var isNameOk: Boolean,
+        var isPasswordOk: Boolean
+    )
+
+    private val _checkResult = LiveEvent<LoginInput>()
+    val checkResult: LiveData<LoginInput> get() = _checkResult
+
+    //TODO: is_loggedinのレスポンスを格納するLiveData
+
+    //TODO: loginのレスポンスを格納するLiveData
+
+    fun checkAndTrim(inputData: LoginInput) {
+        val trimName = inputData.name.trim()
+        if (trimName == "") {
+            inputData.isNameOk = false
+        } else {
+            inputData.name = trimName
+            inputData.isNameOk = true
+        }
+        inputData.isPasswordOk = inputData.password != ""
+        _checkResult.postValue(inputData)
+    }
+
+    //TODO: is_loggedinのGET
+
+    //TODO: loginのPOST
+
+    //TODO: login結果を受けて、roomにデータを格納する
 }
