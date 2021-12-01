b0cbe6a96ff12801e8c96d49ab5024b636f56f0a
Sorasky4 <kacky1210@gmail.com>
2021-12-01 15:18:58 +0900

    Add: view, viewModelでログアウトに必要な処理

--- app/src/main/java/com/websarva/wings/android/flat/view/UserSettingFragment.kt
+++ app/src/main/java/com/websarva/wings/android/flat/view/UserSettingFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.websarva.wings.android.flat.FLATApplication.Companion.myId
 import com.websarva.wings.android.flat.R
 import com.websarva.wings.android.flat.databinding.FragmentLoginBinding
 import com.websarva.wings.android.flat.databinding.FragmentUserSettingBinding
@@ -15,14 +17,14 @@ import com.websarva.wings.android.flat.viewmodel.LoginViewModel
 import com.websarva.wings.android.flat.viewmodel.UserSettingViewModel
 
 class UserSettingFragment : Fragment() {
-    private val viewModel: LoginViewModel by viewModels()
+    private val viewModel: UserSettingViewModel by viewModels()
     private var _binding: FragmentUserSettingBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentUserSettingBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -32,9 +34,28 @@ class UserSettingFragment : Fragment() {
 
         binding.apply {
             btLogout.setOnSafeClickListener {
-                //TODO: logout
+                viewModel.logout(myId)
             }
         }
+
+        viewModel.logoutResponse.observe(viewLifecycleOwner, {
+            when (it.code()) {
+                200 -> {
+                    viewModel.deleteUserData()
+                }
+                else -> {
+                    //TODO: Logout失敗
+                }
+            }
+        })
+
+        viewModel.roomChanged.observe(viewLifecycleOwner, {
+            //TODO: 画面遷移
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, {
+            Toast.makeText(activity, getString(R.string.connection_error), Toast.LENGTH_SHORT).show()
+        })
     }
 
 }
