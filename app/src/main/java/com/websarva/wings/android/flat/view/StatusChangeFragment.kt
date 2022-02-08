7cc1bca3a05ca7a51da17df1d6a2023f58264366
sora <kacky1210@gmail.com>
2022-02-09 02:48:05 +0900

    Add: とりあえずステータス選択画面が見えるようになったので

--- app/src/main/java/com/websarva/wings/android/flat/view/StatusChangeFragment.kt
+++ app/src/main/java/com/websarva/wings/android/flat/view/StatusChangeFragment.kt
@@ -6,13 +6,45 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.websarva.wings.android.flat.R
+import com.websarva.wings.android.flat.databinding.FragmentStatusChangeBinding
 
 class StatusChangeFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentStatusChangeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_status_change, container, false)
+    ): View? {
+        _binding = FragmentStatusChangeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.layoutAtSchool.apply {
+            ivSelectStatus.setImageResource(R.drawable.ic_at_school)
+            tvSelectStatus.text = getString(R.string.at_school)
+        }
+        binding.layoutFree.apply {
+            ivSelectStatus.setImageResource(R.drawable.ic_free)
+            tvSelectStatus.text = getString(R.string.free)
+        }
+        binding.layoutBusy.apply {
+            ivSelectStatus.setImageResource(R.drawable.ic_busy)
+            tvSelectStatus.text = getString(R.string.busy)
+        }
+        binding.layoutNotAtSchool.apply {
+            ivSelectStatus.setImageResource(R.drawable.ic_not_at_school)
+            tvSelectStatus.text = getString(R.string.not_at_school)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     companion object {
         const val TAG = "StatusChangeFragment"
