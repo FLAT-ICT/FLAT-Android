8301f55999c4dd55025c27a3ba8036aa027f5090
sora <kacky1210@gmail.com>
2022-02-10 02:03:13 +0900

    Add: 相手のステータスが表示されるようにした

--- app/src/main/java/com/websarva/wings/android/flat/view/ViewExtension.kt
+++ app/src/main/java/com/websarva/wings/android/flat/view/ViewExtension.kt
@@ -1,7 +1,10 @@
 package com.websarva.wings.android.flat.view
 
 import android.view.View
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.websarva.wings.android.flat.R
 
 @BindingAdapter("android:onSafeClick")
 fun View.setOnSafeClickListener(listener: View.OnClickListener) {
@@ -19,5 +22,15 @@ fun View.setOnSafeClickListener(listener: View.OnClickListener) {
         // 押下処理を実行
         listener.onClick(this)
         return@setOnClickListener
+    }
+}
+
+@BindingAdapter("android:setStatus")
+fun setStatus(view: ImageView, status: Int) {
+    when(status) {
+        0 -> view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_not_at_school))
+        1 -> view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_at_school))
+        2 -> view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_free))
+        3 -> view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_busy))
     }
 }
