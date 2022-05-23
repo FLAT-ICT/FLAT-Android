6147a76fbeee2dd947c8ff2ffc05adcee98b0e7e
atomic928 <64305950+atomic928@users.noreply.github.com>
2022-05-23 16:28:39 +0900

    Map画面の追加

---
+++ app/src/main/java/com/websarva/wings/android/flat/view/MapFragment.kt
@@ -0,0 +1,33 @@
+package com.websarva.wings.android.flat.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.websarva.wings.android.flat.R
+import com.websarva.wings.android.flat.viewmodel.MapViewModel
+
+class MapFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MapFragment()
+    }
+
+    private lateinit var viewModel: MapViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
