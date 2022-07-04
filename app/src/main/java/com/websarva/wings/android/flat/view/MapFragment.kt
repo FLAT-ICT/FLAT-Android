50c285d3d68cb1d642f1f84580287f8c6ebdfe66
cathiecode <cathiecode@gmail.com>
2022-07-04 17:10:31 +0900

    WIP

--- app/src/main/java/com/websarva/wings/android/flat/view/MapFragment.kt
+++ app/src/main/java/com/websarva/wings/android/flat/view/MapFragment.kt
@@ -1,41 +1,76 @@
 package com.websarva.wings.android.flat.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.Fragment
 import com.google.android.material.composethemeadapter.MdcTheme
+import com.mxalbert.zoomable.Zoomable
 import com.websarva.wings.android.flat.R
+import com.websarva.wings.android.flat.ui.MapPOCTheme
 
-class MyActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MapFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         super.onCreate(savedInstanceState)
-        // ...
-
-        val greeting = findViewById<ComposeView>(R.id.map)
-        greeting.setContent {
-            MdcTheme { // or AppCompatTheme
-                Greeting()
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MapPOCTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        Map()
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-private fun Greeting() {
-    Text(
-        text = "Hello, world!",
-        style = MaterialTheme.typography.h5,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-    )
+fun Map() {
+    Zoomable {
+        val painter = painterResource(R.drawable.map)
+        val size = painter.intrinsicSize
+        Image(
+            painter = painter,
+            contentDescription = "Map",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .aspectRatio(size.width / size.height)
+        )
+        Image(
+            painter = painterResource(R.drawable.pin),
+            contentDescription = "Pin",
+            modifier = Modifier.absoluteOffset(50.dp, 200.dp)
+        )
+    }
 }
