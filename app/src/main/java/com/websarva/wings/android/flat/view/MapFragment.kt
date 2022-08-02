1e7df0bf49234654c2b45024e254544a90875b5e
atomic928 <64305950+atomic928@users.noreply.github.com>
2022-08-02 17:42:00 +0900

    自分の居場所がピンをさされるようにした

--- app/src/main/java/com/websarva/wings/android/flat/view/MapFragment.kt
+++ app/src/main/java/com/websarva/wings/android/flat/view/MapFragment.kt
@@ -1,14 +1,24 @@
 package com.websarva.wings.android.flat.view
 
+import android.app.ActionBar
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.marginBottom
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.websarva.wings.android.flat.R
 import com.websarva.wings.android.flat.databinding.FragmentMapBinding
 import com.websarva.wings.android.flat.viewmodel.MapViewModel
+import org.json.JSONException
+import org.json.JSONObject
+import org.w3c.dom.Node
+import android.view.ViewGroup.LayoutParams
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MapFragment : Fragment() {
     private val viewModel: MapViewModel by viewModels()
@@ -26,6 +36,141 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getUserData()
+        viewModel.user.observe(viewLifecycleOwner) {
+            binding.apply {
+                if (it.spot == "132教員室") {
+                    ivPin0.visibility = View.VISIBLE
+                } else {
+                    ivPin0.visibility = View.INVISIBLE
+                }
+                if (it.spot == "364コンピュータ教室") {
+                    ivPin1.visibility = View.VISIBLE
+                } else {
+                    ivPin1.visibility = View.INVISIBLE
+                }
+                if (it.spot == "363コンピュータ教室") {
+                    ivPin2.visibility = View.VISIBLE
+                } else {
+                    ivPin2.visibility = View.INVISIBLE
+                }
+                if (it.spot == "工房") {
+                    ivPin3.visibility = View.VISIBLE
+                } else {
+                    ivPin3.visibility = View.INVISIBLE
+                }
+                if (it.spot == "体育館") {
+                    ivPin4.visibility = View.VISIBLE
+                } else {
+                    ivPin4.visibility = View.INVISIBLE
+                }
+                if (it.spot == "トレーニングルーム") {
+                    ivPin5.visibility = View.VISIBLE
+                } else {
+                    ivPin5.visibility = View.INVISIBLE
+                }
+                if (it.spot == "367大講義室") {
+                    ivPin6.visibility = View.VISIBLE
+                } else {
+                    ivPin6.visibility = View.INVISIBLE
+                }
+                if (it.spot == "368大講義室") {
+                    ivPin7.visibility = View.VISIBLE
+                } else {
+                    ivPin7.visibility = View.INVISIBLE
+                }
+                if (it.spot == "332教員室") {
+                    ivPin8.visibility = View.VISIBLE
+                } else {
+                    ivPin8.visibility = View.INVISIBLE
+                }
+                if (it.spot == "社会連携センタ") {
+                    ivPin9.visibility = View.VISIBLE
+                } else {
+                    ivPin9.visibility = View.INVISIBLE
+                }
+                if (it.spot == "ミュージアム") {
+                    ivPin10.visibility = View.VISIBLE
+                } else {
+                    ivPin10.visibility = View.INVISIBLE
+                }
+                if (it.spot == "モール") {
+                    ivPin11.visibility = View.VISIBLE
+                } else {
+                    ivPin11.visibility = View.INVISIBLE
+                }
+                if (it.spot == "3階エントランスホール") {
+                    ivPin12.visibility = View.VISIBLE
+                } else {
+                    ivPin12.visibility = View.INVISIBLE
+                }
+                if (it.spot == "127教員室") {
+                    ivPin13.visibility = View.VISIBLE
+                } else {
+                    ivPin13.visibility = View.INVISIBLE
+                }
+                if (it.spot == "ラウンジ1階西側") {
+                    ivPin14.visibility = View.VISIBLE
+                } else {
+                    ivPin14.visibility = View.INVISIBLE
+                }
+                if (it.spot == "124教員室") {
+                    ivPin15.visibility = View.VISIBLE
+                } else {
+                    ivPin15.visibility = View.INVISIBLE
+                }
+                if (it.spot == "プレゼンベイ／スタジオ1階") {
+                    ivPin16.visibility = View.VISIBLE
+                } else {
+                    ivPin16.visibility = View.INVISIBLE
+                }
+                if (it.spot == "食堂") {
+                    ivPin17.visibility = View.VISIBLE
+                } else {
+                    ivPin17.visibility = View.INVISIBLE
+                }
+                if (it.spot == "126教員室") {
+                    ivPin18.visibility = View.VISIBLE
+                } else {
+                    ivPin18.visibility = View.INVISIBLE
+                }
+                if (it.spot == "1階エントランスホール") {
+                    ivPin19.visibility = View.VISIBLE
+                } else {
+                    ivPin19.visibility = View.INVISIBLE
+                }
+                if (it.spot == "売店") {
+                    ivPin20.visibility = View.VISIBLE
+                } else {
+                    ivPin20.visibility = View.INVISIBLE
+                }
+                if (it.spot == "223教員室") {
+                    ivPin21.visibility = View.VISIBLE
+                } else {
+                    ivPin21.visibility = View.INVISIBLE
+                }
+
+            }
+        }
+
+        binding.btMap1.setOnClickListener{
+            binding.ivMap.setImageResource(R.drawable.map1f)
+        }
+        binding.btMap2.setOnClickListener{
+            binding.ivMap.setImageResource(R.drawable.map2f)
+        }
+        binding.btMap3.setOnClickListener{
+            binding.ivMap.setImageResource(R.drawable.map3f)
+        }
+        binding.btMap4.setOnClickListener{
+            binding.ivMap.setImageResource(R.drawable.map4f)
+        }
+        binding.btMap5.setOnClickListener{
+            binding.ivMap.setImageResource(R.drawable.map5f)
+        }
+
 
     }
 }
