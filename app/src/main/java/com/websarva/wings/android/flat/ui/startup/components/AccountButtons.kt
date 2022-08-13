25c66fb279cd62adc3fdea11ebe8cd9afdbe90bb
t4t5u0 <ymmtryk0902@gmail.com>
2022-08-14 00:09:22 +0900

    Update: ボタンの色を正しく参照できるようにした．先にアクティビティ周りの作業をするべきだと思うのでそっちをやる

--- app/src/main/java/com/websarva/wings/android/flat/ui/startup/components/AccountButtons.kt
+++ app/src/main/java/com/websarva/wings/android/flat/ui/startup/components/AccountButtons.kt
@@ -2,8 +2,10 @@ package com.websarva.wings.android.flat.ui.startup.components
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.textButtonColors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.websarva.wings.android.flat.ui.theme.FLATTheme
 
@@ -19,11 +21,15 @@ fun LoginButton() {
         onClick = { /*TODO*/ },
         shape = RoundedCornerShape(4.dp),
 //        colors = FLATTheme.colors.primary
-        colors = FLATTheme.colors.primary
+        colors = textButtonColors(
+            backgroundColor = FLATTheme.colors.primary,
+            contentColor = Color.Black
+        )
     ) {
         Text("ログイン")
     }
 }
+
 
 @Composable
 fun RegisterButton() {
