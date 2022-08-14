22b6f2ad322cd5566164b1d45628a790a277062e
t4t5u0 <ymmtryk0902@gmail.com>
2022-08-15 08:32:13 +0900

    Update: アカウント登録に必要コンテンツをざっくり配置な

--- app/src/main/java/com/websarva/wings/android/flat/ui/startup/accountRegistration/AccountRegistrationScreen.kt
+++ app/src/main/java/com/websarva/wings/android/flat/ui/startup/accountRegistration/AccountRegistrationScreen.kt
@@ -1,4 +1,102 @@
 package com.websarva.wings.android.flat.ui.startup.accountRegistration
 
-class AccountRegistrationScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.websarva.wings.android.flat.ui.theme.FLATTheme
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AccountRegistrationScreen() {
+    var name by remember { mutableStateOf("") }
+    var pass1 by remember { mutableStateOf("") }
+    var pass2 by remember { mutableStateOf("") }
+
+    FLATTheme{
+        Surface {
+            Column(modifier = Modifier.fillMaxSize()) {
+                NameTextField(value = name){name=it}
+                PasswordTextField(password = pass1){pass1=it}
+                PasswordTextField(password = pass2){pass2=it}
+                RegisterButton(name = name, pass1 = pass1, pass2 = pass2)
+            }
+        }
+    }
+}
+
+@Composable
+fun NameTextField(value: String, onValueChange: (String) -> Unit) {
+    val focusManager = LocalFocusManager.current
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
+,        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }
+        ),
+        label = {
+            Text("Name")
+        }
+
+    )
+}
+
+@Composable
+fun PasswordTextField(password: String, onValueChange: (String) -> Unit) {
+//    var password by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+    TextField(
+        value = password,
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text("Enter password") },
+        visualTransformation =
+        if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                val visibilityIcon =
+                    if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                // Please provide localized description for accessibility services
+                val description = if (passwordHidden) "Show password" else "Hide password"
+                Icon(imageVector = visibilityIcon, contentDescription = description)
+            }
+        }
+    )
+}
+
+
+@Composable
+fun RegisterButton(name: String, pass1: String, pass2: String) {
+    Button(onClick = {}) {
+        if (name.isNotEmpty() && pass1.isNotEmpty() && pass2.isNotEmpty()) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AccountRegistrationScreenPreview() {
+    AccountRegistrationScreen()
 }
