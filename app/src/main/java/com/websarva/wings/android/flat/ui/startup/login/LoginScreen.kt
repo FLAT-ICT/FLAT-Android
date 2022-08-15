53e1ac2ba0d359f0377dab637e35402e8141c2f2
t4t5u0 <ymmtryk0902@gmail.com>
2022-08-16 06:32:59 +0900

    Update: デフォルトで存在しないマテリアルアイコンをインポート

--- app/src/main/java/com/websarva/wings/android/flat/ui/startup/login/LoginScreen.kt
+++ app/src/main/java/com/websarva/wings/android/flat/ui/startup/login/LoginScreen.kt
@@ -2,9 +2,21 @@ package com.websarva.wings.android.flat.ui.startup.login
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import com.websarva.wings.android.flat.ui.theme.FLATTheme
 
 @Composable
@@ -13,11 +25,43 @@ fun LoginScreen() {
         Surface() {
             Column(modifier = Modifier.fillMaxSize()) {
                 // Text: ログイン画面 // これ多分別のテキスト
+                Text(text = "ログイン")
                 // CustomTextField: ユーザー名
                 // CustomTextField: パスワード
+                PasswordTextField()
                 // Button: ログイン
                 // Text: アカウントを新規登録する
             }
         }
     }
+}
+
+@Composable
+fun PasswordTextField() {
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+    TextField(
+        value = password,
+        onValueChange = { password = it },
+        singleLine = true,
+        label = { Text("Enter password") },
+        visualTransformation =
+        if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                val visibilityIcon =
+                    if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                // Please provide localized description for accessibility services
+                val description = if (passwordHidden) "Show password" else "Hide password"
+                Icon(imageVector = visibilityIcon, contentDescription = description)
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen()
 }
