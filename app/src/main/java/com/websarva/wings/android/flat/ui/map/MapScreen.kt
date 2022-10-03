cbb8f2f108e4ad7ac8d23a8cbeb97753eb7cea57
funera1 <60760935+funera1@users.noreply.github.com>
2022-10-03 18:06:49 +0900

    update: zoomable追加

--- app/src/main/java/com/websarva/wings/android/flat/ui/map/MapScreen.kt
+++ app/src/main/java/com/websarva/wings/android/flat/ui/map/MapScreen.kt
@@ -1,15 +1,19 @@
 package com.websarva.wings.android.flat.ui.map
 
+import com.websarva.wings.android.flat.R
+import androidx.compose.foundation.Image
 import android.view.Surface
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mxalbert.zoomable.Zoomable
 import com.websarva.wings.android.flat.ui.theme.FLATTheme
 
 /*
@@ -19,5 +23,25 @@ import com.websarva.wings.android.flat.ui.theme.FLATTheme
 fun MapScreen (){
     FLATTheme {
         // この中にmapの画面を作成していく
+//        val state = rememberZoomableState(
+//            minScale = 2f
+//        )
+//        Zoomable(state = state) {
+//            Text(text = "Zoom me!")
+//        }
+
+        Column(modifier = Modifier.fillMaxSize()){
+            Zoomable {
+                val painter = painterResource(id = R.drawable.map1f)
+                val size = painter.intrinsicSize
+                Image(
+                    painter = painter,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .aspectRatio(size.width / size.height)
+                        .fillMaxSize()
+                )
+            }
+        }
     }
 }
