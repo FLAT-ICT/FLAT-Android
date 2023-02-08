a9acee6ce6080c3008f059b3e671e80d5b4217e0
t4t5u0 <ymmtryk0902@gmail.com>
2023-02-09 04:16:09 +0900

    Wip: Mapの要求仕様を書いた．階数をしていできるようにしたい，

--- app/src/main/java/com/websarva/wings/android/flat/ui/map/MapScreen.kt
+++ app/src/main/java/com/websarva/wings/android/flat/ui/map/MapScreen.kt
@@ -4,9 +4,10 @@ import com.websarva.wings.android.flat.R
 import androidx.compose.foundation.Image
 import android.view.Surface
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -15,12 +16,29 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mxalbert.zoomable.Zoomable
 import com.websarva.wings.android.flat.ui.theme.FLATTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 /*
     mapの画面を作成する関数
+
+    Mapの拡大率をStateとして持つ
+    画面をピンチイン・ピンチアウトで拡大・縮小できるようにする
+
+    1つの場所に複数のピンがある場合，現在の拡大率に応じて表示方法を変える．
+    - 拡大率が閾値より小さい場合，ピンをまとめて表示する．ピンには数字を表示する．
+    - 拡大率が閾値より大きい場合，ピンを個別に表示する．
+
+    ピンは地図に対する相対表示位置を持つ．
 */
 @Composable
 fun MapScreen (){
+
+    val floor = remember { mutableStateOf(1) }
+    var mapScale = remember { mutableStateOf(1f) }
+
+    val scope = rememberCoroutineScope()
+
     FLATTheme {
         // この中にmapの画面を作成していく
 //        val state = rememberZoomableState(
@@ -30,18 +48,125 @@ fun MapScreen (){
 //            Text(text = "Zoom me!")
 //        }
 
-        Column(modifier = Modifier.fillMaxSize()){
-            Zoomable {
-                val painter = painterResource(id = R.drawable.map1f)
-                val size = painter.intrinsicSize
-                Image(
-                    painter = painter,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .aspectRatio(size.width / size.height)
-                        .fillMaxSize()
-                )
+        Row() {
+            elevatorButton(floor, scope)
+            Column(modifier = Modifier.fillMaxSize()) {
+                when (floor.value) {
+                    1 -> map1f()
+                    2 -> map2f()
+                    3 -> map3f()
+                    4 -> map4f()
+                    5 -> map5f()
+                }
             }
         }
     }
+}
+
+
+@Composable
+fun elevatorButton(currentFloor: MutableState<Int>, scope: CoroutineScope){
+    Row(){
+        Button(onClick = {
+            scope.launch {
+                currentFloor.value = 1
+            }
+        }) {
+            Text(text = "1")
+        }
+        Button(onClick = {
+            scope.launch {
+                currentFloor.value = 2
+            }
+        }) {
+            Text(text = "2")
+        }
+        Button(onClick = {
+            scope.launch {
+                currentFloor.value = 3
+            }
+        }) {
+            Text(text = "3")
+        }
+        Button(onClick = {
+            scope.launch {
+                currentFloor.value = 4
+            }
+        }) {
+            Text(text = "4")
+        }
+        Button(onClick = {
+            scope.launch {
+                currentFloor.value = 5
+            }
+        }) {
+            Text(text = "5")
+        }
+    }
+
+}
+
+@Composable
+fun map1f(){
+    val painter = painterResource(id = R.drawable.map1f)
+    val size = painter.intrinsicSize
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = Modifier
+            .aspectRatio(size.width / size.height)
+            .fillMaxSize()
+    )
+}
+
+@Composable
+fun map2f(){
+    val painter = painterResource(id = R.drawable.map2f)
+    val size = painter.intrinsicSize
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = Modifier
+            .aspectRatio(size.width / size.height)
+            .fillMaxSize()
+    )
+}
+
+@Composable
+fun map3f(){
+    val painter = painterResource(id = R.drawable.map3f)
+    val size = painter.intrinsicSize
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = Modifier
+            .aspectRatio(size.width / size.height)
+            .fillMaxSize()
+    )
+}
+
+@Composable
+fun map4f(){
+    val painter = painterResource(id = R.drawable.map4f)
+    val size = painter.intrinsicSize
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = Modifier
+            .aspectRatio(size.width / size.height)
+            .fillMaxSize()
+    )
+}
+
+@Composable
+fun map5f(){
+    val painter = painterResource(id = R.drawable.map5f)
+    val size = painter.intrinsicSize
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = Modifier
+            .aspectRatio(size.width / size.height)
+            .fillMaxSize()
+    )
 }
